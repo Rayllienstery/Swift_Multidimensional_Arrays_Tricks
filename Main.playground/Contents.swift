11533d9d82a5ff4a06aa6fc61563fa894060ca82
Rayllienstery <rayleefx@gmail.com>
2017-10-18 07:37:29 +0300

    Added find min and max values in Int lines

--- Main.playground/Contents.swift
+++ Main.playground/Contents.swift
@@ -1,10 +1,29 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+//Some random value lists
+var Matrix : [[Int]] = [[5,16,2,26,5],[23,3,1,43,254,1,0,-25],[0,255,123,3,1,26]]
 
-var test : [[String]] = [["1","2","3"],["4","5"],["6","17"]]
+//Array, what contains MAX value on every line
+var maxValues : [Int] = Array(repeating: 0, count: Matrix.count)
+for i in 0..<Matrix.count{
+    maxValues[i] = Matrix[i][0]
+    for j in 1..<Matrix[i].count{
+        if maxValues[i] < Matrix[i][j]{
+            maxValues[i] = Matrix[i][j]
+        }
+    }
+}
+print("max values: ", maxValues)
 
-var test2 : String = test[0][2]
-
-var test3 : String = test[test.count-1][test[test.count-1].count-1]
-
+//Array what contains MIN value on every line
+var minValues : [Int] = Array(repeating: 0, count: Matrix.count)
+for i in 0..<Matrix.count{
+    minValues[i] = Matrix[i][0]
+    for j in 1..<Matrix[i].count{
+        if minValues[i] > Matrix[i][j]{
+            minValues[i] = Matrix[i][j]
+        }
+    }
+}
+print("min values: ", minValues)
