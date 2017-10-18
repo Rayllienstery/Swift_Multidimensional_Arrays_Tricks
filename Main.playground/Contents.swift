721595a5a70a2e52260de598fb4cd3d004b20078
Rayllienstery <rayleefx@gmail.com>
2017-10-18 06:27:39 +0300

    Initialize

---
+++ Main.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var test : [[String]] = [["1","2","3"],["4","5"],["6","17"]]
+
+var test2 : String = test[0][2]
+
+var test3 : String = test[test.count-1][test[test.count-1].count-1]
+
